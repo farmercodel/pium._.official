3307c93de73537b66489d7952ab29a3f3de2f5c7
lee0su <leeys4903@naver.com>
2025-08-10 18:11:28 +0900

    chore(frontend): setup frontend project with Docker

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full h-screen bg-gray-500">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
