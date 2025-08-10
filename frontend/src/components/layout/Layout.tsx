5e21778c465a06edd17c36747c88728dea1128b1
lee0su <leeys4903@naver.com>
2025-08-10 20:32:44 +0900

    feat: Layout 틀 추가

---
+++ frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from './Header.tsx'
+import Footer from './Footer.tsx'
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+    
+    return (
+        <div className="min-h-screen flex flex-col">
+            {/* Header */}
+            <Header />
+
+            {/* Main */}
+            <main className="flex-1 pt-16">
+                {children}
+            </main>
+
+            {/* Footer */}
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
