9eebcb2728019fa0227805add62bd06c06f732d2
lee0su <leeys4903@naver.com>
2025-08-17 15:21:50 +0900

    style(frontend): InputSection, PreviewCardSection Constant 분리

--- frontend/src/components/survey/PreviewCardSection.tsx
+++ frontend/src/components/survey/PreviewCardSection.tsx
@@ -1,7 +1,9 @@
+import { PREVIEW_CARD_SECTION_CONTENT } from "../../constants/surveyConstant"
+
 const PreviewCardSection = () => {
     return (
         <div className="hidden md:block md:col-span-2 p-6 bg-green-500 w-full h-full">
-            프리뷰 카드
+            {PREVIEW_CARD_SECTION_CONTENT.title}
         </div>
     )
 }
---
+++ frontend/src/constants/surveyConstant.ts
@@ -0,0 +1,117 @@
+{/** 
+    <div className="col-span-5 md:col-span-3 p-6 w-full gap-2 flex flex-col overflow-y-auto">
+            <p className="text-sm text-gray-500"><span className="text-red-500">*</span> 표시는 필수 항목입니다.</p>
+            <InputBox placeholder="답변을 입력해주세요." title="가게명" required />
+            <InputBox placeholder="답변을 입력해주세요." title="지역 위치/상권 키워드" required />
+            <InputBox placeholder="답변을 입력해주세요." title="가게 주소" required />
+            <InputBox placeholder="답변을 입력해주세요." title="가격대" required />
+            <InputBox placeholder="답변을 입력해주세요." title="영업 시간 정보" required />
+            <InputBox placeholder="답변을 입력해주세요." title="가게 업종" required />
+            <InputBox placeholder="답변을 입력해주세요." title="가격 소개" required />
+            <InputBox placeholder="답변을 입력해주세요." title="답변 톤" required />
+            <InputBox placeholder="답변을 입력해주세요." title="이미지 파일" required />
+            <div className="border-b border-b-3 border-gray-300 w-full my-4"></div>
+            <InputBox placeholder="답변을 입력해주세요." title="참고 링크" />
+            <InputBox placeholder="답변을 입력해주세요." title="제공 제품/서비스 키워드" />
+            <InputBox placeholder="답변을 입력해주세요." title="타깃 고객" />
+            <InputBox placeholder="답변을 입력해주세요." title="가게 인스타그램 ID" />
+            <InputBox placeholder="답변을 입력해주세요." title="진행중인 프로모션" />
+            <InputBox placeholder="답변을 입력해주세요." title="해시태그 개수 상한" />
+        </div>    
+*/}
+
+const INPUT_SECTION_CONTENT = [
+    {
+        title: '가게명',
+        placeholder: '답변을 입력해주세요.',
+        required: true,
+    },
+    {
+        title: '지역 위치/상권 키워드',
+        placeholder: '답변을 입력해주세요.',
+        required: true,
+    },
+
+    {
+        title: '가게 주소',
+        placeholder: '답변을 입력해주세요.',
+        required: true,
+    },
+
+    {
+        title: '가격대',
+        placeholder: '답변을 입력해주세요.',
+        required: true,
+    },
+
+    {
+        title: '영업 시간 정보',
+        placeholder: '답변을 입력해주세요.',
+        required: true,
+    },
+
+    {
+        title: '가게 업종',
+        placeholder: '답변을 입력해주세요.',
+        required: true,
+    },
+
+    {
+        title: '가격 소개',
+        placeholder: '답변을 입력해주세요.',
+        required: true,
+    },
+
+    {
+        title: '답변 톤',
+        placeholder: '답변을 입력해주세요.',
+        required: true,
+    },
+
+    {
+        title: '이미지 파일',
+        placeholder: '답변을 입력해주세요.',
+        required: true,
+    },
+    {
+        title: '참고 링크',
+        placeholder: '답변을 입력해주세요.',
+        required: false,
+    },
+    {
+        title: '제공 제품/서비스 키워드',
+        placeholder: '답변을 입력해주세요.',
+        required: false,
+    },
+
+    {
+        title: '타깃 고객',
+        placeholder: '답변을 입력해주세요.',
+        required: false,
+    },
+
+    {
+        title: '가게 인스타그램 ID',
+        placeholder: '답변을 입력해주세요.',
+        required: false,
+    },
+
+    {
+        title: '진행중인 프로모션',
+        placeholder: '답변을 입력해주세요.',
+        required: false,
+    },
+
+    {
+        title: '해시태그 개수 상한',
+        placeholder: '답변을 입력해주세요.',
+        required: false,
+    },
+
+] as const
+
+const PREVIEW_CARD_SECTION_CONTENT ={
+        title: '프리뷰 카드',
+} as const
+
+export { INPUT_SECTION_CONTENT, PREVIEW_CARD_SECTION_CONTENT }
