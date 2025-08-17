bc2882a88634d6406599312160b76d7918adb45d
kimtaeyeon <kty20040308@gmail.com>
2025-08-18 01:48:27 +0900

    메인페이지

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,12 +1,28 @@
 import PageLayout from "../components/common/PageLayout"
+import About from "/dev/pium._.official-new/frontend/public/aboutpage.jpg"
 
-{/** 팀, 웹 소개 페이지 */}
 const AboutPage = () => {
-    return (
-        <PageLayout>
-            <h1>AboutPage</h1>
-        </PageLayout>
-    )
+  return (
+    <PageLayout>
+      <div className="flex items-start gap-10 ml-10 mt-10 ">
+        {/* 왼쪽 텍스트 */}
+        <div>
+          <h1 className="text-[70px] font-bold">Project</h1>
+          <h2 className="text-[20px] mt-5">
+            피움은 지역 소상공인 디지털 마케팅 진입 장벽을 낮추는 AI 기반 웹 서비스입니다.
+          </h2>
+        </div>
+
+        {/* 오른쪽 이미지 */}
+        <img
+          src={About}
+          alt="Photo"
+          className="w-[500px] h-[500px] object-cover"
+          style={{ marginRight: "-1000px" }}
+        />
+      </div>
+    </PageLayout>
+  )
 }
 
 export default AboutPage
