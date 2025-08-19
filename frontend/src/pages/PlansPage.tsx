6c190a5cfe682f5bc09f940b8f438197566b288e
lee0su <leeys4903@naver.com>
2025-08-19 22:46:49 +0900

    feat(frontend): PlansPage 구조잡기

--- frontend/src/pages/PlansPage.tsx
+++ frontend/src/pages/PlansPage.tsx
@@ -1,10 +1,61 @@
 import PageLayout from "../components/common/PageLayout"
+import Button from "../components/common/Button"
 
-{/** 구독 플랜 페이지 */}
+{/** 구독 플랜 페이지 */ }
 const PlansPage = () => {
     return (
         <PageLayout>
-            <h1>PlansPage</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 lg:gap-12 p-6 lg:p-12 h-[calc(100vh-72px-132px)]">
+                {/* Plan Card */}
+                <div className="p-6 rounded-2xl shadow-lg border flex flex-col h-full justify-center hover:scale-105 transition-all duration-300">
+                    <h3 className="text-xl font-semibold">Free</h3>
+                    <p className="text-3xl font-bold mt-2">₩0<span className="text-sm">/월</span></p>
+                    <ul className="mt-4 space-y-2 text-gray-600">
+                        <li>✔ 기능 A</li>
+                        <li>✘ 기능 B</li>
+                        <li>✘ 기능 C</li>
+                    </ul>
+                    <Button
+                        variant="primary"
+                        fullWidth
+                        className="mt-auto"
+                    >
+                        시작하기
+                    </Button>
+                </div>
+                <div className="p-6 rounded-2xl shadow-lg border flex flex-col h-fulljustify-center hover:scale-105 transition-all duration-300">
+                    <h3 className="text-xl font-semibold">Basic</h3>
+                    <p className="text-3xl font-bold mt-2">₩9,900<span className="text-sm">/월</span></p>
+                    <ul className="mt-4 space-y-2 text-gray-600">
+                        <li>✔ 기능 A</li>
+                        <li>✔ 기능 B</li>
+                        <li>✘ 기능 C</li>
+                    </ul>
+                    <Button
+                        variant="primary"
+                        fullWidth
+                        className="mt-auto"
+                    >
+                        시작하기
+                    </Button>
+                </div>
+                <div className="p-6 rounded-2xl shadow-lg border flex flex-col h-full justify-center hover:scale-105 transition-all duration-300">
+                    <h3 className="text-xl font-semibold">Pro</h3>
+                    <p className="text-3xl font-bold mt-2">₩19,900<span className="text-sm">/월</span></p>
+                    <ul className="my-4 space-y-2 text-gray-600">
+                        <li>✔ 기능 A</li>
+                        <li>✔ 기능 B</li>
+                        <li>✔ 기능 C</li>
+                    </ul>
+                    <Button
+                        variant="primary"
+                        fullWidth
+                        className="mt-auto"
+                    >
+                        시작하기
+                    </Button>
+                </div>
+            </div>
         </PageLayout>
     )
 }
