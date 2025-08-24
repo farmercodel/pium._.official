b2792ad58065ed6925c3e40970aa8fb197cc706c
kimtaeyeon <kty20040308@gmail.com>
2025-08-24 09:56:30 +0900

    연동

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "",
+  baseURL: "import.meta.env.VITE_API_URL",
   timeout: 60000,
 });
 
---
+++ frontend/src/api/inquiry.ts
@@ -0,0 +1,7 @@
+// src/api/api.ts
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "http://127.0.0.1:5173", // 여기를 실제 백엔드 주소로 변경
+  timeout: 60000,
+});
